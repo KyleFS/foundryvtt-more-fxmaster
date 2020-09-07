fc05129844db6f93cb11f94bfd8c135452e8b0a6
Kyle Spiller <me@kylespiller.com>
2020-09-07 00:35:32 -0400

    Add Flexbloom and add reset hook

--- scripts/more-fxmaster.js
+++ scripts/more-fxmaster.js
@@ -1,14 +1,22 @@
+import { filterManager } from "../../fxmaster/filters/FilterManager.js";
+
 import { FXGodrayFilter } from "./filters_FXGodrayFilter.js";
+import { FXFlexBloomFilter } from "./filters_FXFlexBloomFilter.js";
 
 Hooks.on("init", () => {
     // Adding filters and effects
     mergeObject(CONFIG.fxmaster, {
         filters: {
             godray: FXGodrayFilter,
+            flexbloom: FXFlexBloomFilter,
         }
     });
 });
 
-Hooks.on('switchFilter', () => {
-    canvas.tokens.filters = [];
+Hooks.on("mfxReset", () => {
+    filterManager.clear();
+    filterManager.removeAll();
+
+    canvas.scene.unsetFlag("fxmaster", "effects");
+    canvas.scene.unsetFlag("fxmaster", "filters");
 });
